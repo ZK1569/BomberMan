183885ce799a6affc4b4482835908e5b4b268dca
ZK1569 <cristian.ursu.2001@gmail.com>
2022-10-10 13:07:55 +0200

    debug win

---
+++ cmake-build-debug/CMakeFiles/BomberMan.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BomberMan.exe"
+  "BomberMan.exe.manifest"
+  "BomberMan.pdb"
+  "CMakeFiles/BomberMan.dir/Code/main.c.obj"
+  "CMakeFiles/BomberMan.dir/Code/main.c.obj.d"
+  "libBomberMan.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BomberMan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BomberMan.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for BomberMan.
+# This may be replaced when dependencies are built.
